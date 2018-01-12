23f8b3a2e2d1336df0218fca97129b9ece7814f4
Olivier Merchiers <omerchiers@gmail.com>
2018-01-12 15:01:35 +0100

    first working version of parallel scripts for scuff-em

--- run/run_on_cluster.jl
+++ run/run_on_cluster.jl
@@ -2,3 +2,16 @@
 Scripts for launching jobs on cluster
 
 """
+
+
+function scuff_job(frequency :: String, geometry_file :: String ,  output_file :: String )
+    output = outputfile*"freq="*frequency*"_radHz"
+    run(`scuff-neq --Geometry $geometry_file --Omega $frequency --EMTPFT --FileBase $output`)
+end
+
+
+function scuff_parallel(frequencies, geometry_file :: String , output_file :: String)
+    freqv = string.(collect(frequencies))
+    scuff_par(frequency) = scuff_job(frequency, geometry_file, output_file)
+    pmap(scuff_par, freqv)
+end

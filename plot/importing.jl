9f08f8981895e6566999e92d39841bfd726d8226
Olivier Merchiers <omerchiers@gmail.com>
2018-02-08 21:06:22 +0100

    Refactored the import_data function by adding a extract_data function. Both functions have two methods for both String and Vector{String}. Added documentation.

--- plot/importing.jl
+++ plot/importing.jl
@@ -1,15 +1,32 @@
 
 
 """
-Import the data from a file *.SIFlux. transf is the label for the transformation. If Only 1 configuration was computed,
-then transf=DEFAULT
+extracts the desired data from a file *.SIFlux.
+# Arguments
+- `transf = "DEFAULT"` : is the label for the transformation if Only 1 configuration was computed.
 
 """
 
-function import_data(filetype :: SIFlux, filename:: String ; transf = "DEFAULT")
+function import_data(filename:: String ; transf = "DEFAULT")
+    df            = DataFrame(readdlm(filename,' ')) # this line will have to be modified with DataFrames 0.11
+    dfPabs,dfPrad = extract_data( df , transf)
+    return dfPabs,dfPrad
+end
 
-    df = DataFrame(readdlm(filename,' ')) # this line will have to be modified with DataFrames 0.11
+function import_data(filename:: Vector{String} ; transf = "DEFAULT")
+    dfPabsv = DataFrame[]
+    dfPradv = DataFrame[]
+    for i = 1:length(filename)
+        dfPabs_temp, dfPrad_temp = import_data(filename[i] ; transf=transf)
+        dfPabsv = push!(dfPabsv,dfPabs_temp)
+        dfPradv = push!(dfPradv,dfPrad_temp)
+    end
+    dfPabs = extract_data(dfPabsv , transf)
+    dfPrad = extract_data(dfPradv , transf)
+    return dfPabs,dfPrad
+end
 
+function extract_data(df :: DataFrame , transf)
     df1 = df[df[:x1] .== transf, :]
     nrows = size(df1, 1)
     ncols = size(df1, 2)
@@ -36,33 +53,15 @@ function import_data(filetype :: SIFlux, filename:: String ; transf = "DEFAULT")
     rename!(dfPabs,colnames,newcolnames)
     rename!(dfPrad,colnames,newcolnames)
 
-
-    return dfPabs,dfPrad
-end
-
-"""
-Import data from parallel calculation.
-If you are in the directory containing the files, just give the
-
-"""
-
-function import_data(filetype :: SIFlux{:parallel}, dirname :: String ; transf = "DEFAULT")
-    files = readdir(dirname)
-    dfPabs,dfPrad = import_files(SIFlux(), files ; trans = transf)
-    return dfPabs,dfPrad
+    return dfPabs, dfPrad
 end
 
 
-function import_files(filetype :: FileType , filenames :: Union{String,Vector{String}} ; transf = "DEFAULT")
-    if typeof(filenames) == String
-        dfPabs, dfPrad = import_data(filetype, filenames ; trans = transf )
-        return dfPabs,dfPrad
-    elseif typeof(filenames) == Vector{String}
-        for i = 1:length(filenames)
-            dfPabsi, dfPradi = import_data(filetype, filenames[i] ; trans = transf )
-            dfPabs = append!(dfPabs,dfPabsi)
-            dfPrad = append!(dfPrad,dfPradi)
-        end
-        return dfPabs,dfPrad
+function extract_data(df :: Vector{DataFrame} , transf)
+    df_temp = df[1]
+    for i =2:length(df)
+        append!(df_temp,df[i])
     end
+    sort!(df_temp, cols = :Freq)
+    return df_temp
 end

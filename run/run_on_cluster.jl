ba44feb715408e8035b70a4a385ea809d6f1c590
Olivier Merchiers <omerchiers@gmail.com>
2018-02-19 16:15:26 +0100

    Added a method to scuff_job to handle a frequency file as an input. Changed the original method scuff_job to take in the frequency as a float. Added documentation to explain the input arguments.

--- run/run_on_cluster.jl
+++ run/run_on_cluster.jl
@@ -11,22 +11,65 @@ end
 =#
 
 """
-Serial job
+    scuff_job(frequency :: Float64, geometry_file :: String ,  output_file :: String )
 
+Serial job for a single frequency given as a float
+# Arguments
+- `frequency :: Float64` : frequency in radHz. Note that to get the real frequncies, one has to multiply by 3e14.
+- `geometry_file :: String` : the *.scuffgeo file
+- `output_file :: String` : string to be added to the *.EMTPFT file
 """
-
-function scuff_job(frequency :: String, geometry_file :: String ,  output_file :: String )
+function scuff_job(frequency :: Float64, geometry_file :: String ,  output_file :: String )
+    freq = string(frequency)
     output = output_file*"freq="*frequency*"_radHz"
     run(`scuff-neq --Geometry $geometry_file --Omega $frequency --EMTPFT --FileBase $output`)
 end
 
-"""
-Parallel job
 
 """
+    scuff_job(frequency :: String, geometry_file :: String ,  output_file :: String )
 
+when using a file containing one or many frequencies
+# Arguments
+- `frequency :: String` : the name of the file containing the frequencies.
+"""
+function scuff_job(frequencyfile :: String, geometry_file :: String ,  output_file :: String )
+    lines  = readlines(frequencyfile)
+    output = output_file*"freq="*lines[1]*"-"*lines[end]
+    run(`scuff-neq --Geometry $geometry_file --OmegaFile $frequency --EMTPFT --FileBase $output`)
+end
+
+
+"""
+    scuff_parallel(frequencies, geometry_file :: String ,  output_file :: String )
+
+Launch parallel computation for a range of frequencies
+# Arguments
+- `frequencies` : range object for the frequencies.
+
+# Examples
+
+```jldoctest
+julia> frequencies = 1e2:1e2:1e3
+100.0:100.0:1000.0
+julia> frequencies = linspace(1e2,1e3,10)
+100.0:100.0:1000.0
+julia> frequencies = logspace(2,3,10)
+10-element Array{Float64,1}:
+  100.0
+  129.155
+  166.81
+  215.443
+  278.256
+  359.381
+  464.159
+  599.484
+  774.264
+ 1000.0
+```
+"""
 function scuff_parallel(frequencies, geometry_file :: String , output_file :: String)
-    freqv = string.(collect(frequencies))
+    freqv = collect(frequencies)
     scuff_par(frequency) = scuff_job(frequency, geometry_file, output_file)
     pmap(scuff_par, freqv)
 end

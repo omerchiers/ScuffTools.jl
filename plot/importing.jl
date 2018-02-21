84d877e5ce14d34e370fdcdaa23efa509ec28fbd
Olivier Merchiers <omerchiers@gmail.com>
2018-02-21 17:47:44 +0100

    Introduced new functions : convert_to_array, write_data and simulation_data.

--- plot/importing.jl
+++ plot/importing.jl
@@ -1,12 +1,10 @@
 
 
 """
-extracts the desired data from a file *.SIFlux.
+extracts the desired data from a file *.EMTPFT.
 # Arguments
 - `transf = "DEFAULT"` : is the label for the transformation if Only 1 configuration was computed.
-
 """
-
 function import_data(filename:: String ; transf = "DEFAULT")
     df            = DataFrame(readdlm(filename,' ')) # this line will have to be modified with DataFrames 0.11
     dfPabs,dfPrad = extract_data( df , transf)
@@ -66,24 +64,105 @@ function extract_data(df :: Vector{DataFrame} , transf)
     return df_temp
 end
 
-function simulation_data(filetype :: SIFlux, filename :: Union{String,Vector{String}}, columnname :: Array{Symbol,1} ,T1,T2, trans = "DEFAULT"; savefile = (false," "))
-    dfPabs,dfPrad = import_data(filename ; transf = trans)
+function convert_to_array(dfPabs,dfPrad,columnname)
     dfPabs[:Freq] = w0.*dfPabs[:Freq]
     dfPrad[:Freq] = w0.*dfPrad[:Freq]
     wv   = dfPabs[:Freq]
     Prad = dfPrad[columnname...]
     Pabs = dfPabs[columnname...]
+    return wv, Pabs, Prad
+end
+
+function write_data(flname,dict)
+   for k in keys(dict)
+        propname = k
+        writetable(flname*"_"*propname*".dat", dict[k])
+    end
+end
+
+"""
+    simulation_data(filetype :: SIFlux,...)
+
+Return and save (optional) the spectral flux and transfer function.
+
+# Arguments
+- `trans = "DEFAULT"` : the number of the transformation, trans = "DEFAULT" if no transformation is given
+- `filename ::  Union{String,Vector{String}}` : can be single filename or an array of filenames for parallel computations
+- `columnname` : column name. Example : [:c12]. Only use one name at the time.
+"""
+function simulation_data(filetype :: SIFlux, filename :: Union{String,Vector{String}}, columnname :: Array{Symbol,1} ,T1,T2, trans = "DEFAULT"; savefile = (false," "))
+    dfPabs,dfPrad  = import_data(filename ; transf = transf)
+    wv, Pabs, Prad = convert_to_array(dfPabs,dfPrad,columnname)
 
     qtrans = zeros(Float64,length(wv))
-    τ      = Prad
-    qtrans = transfer_w.(T1,T2,wv,τ)
+    qtrans = transfer_w.(T1,T2,wv,Prad)
     Qtrans = [wv qtrans]
 
     if savefile[1] == true
-        writetable("Pabs_"*savefile[2]*".dat", dfPabs)
-        writetable("Prad_"*savefile[2]*".dat", dfPrad)
         dfQtrans = DataFrame(Qtrans)
-        writetable("Qtrans_"*savefile[2]*".dat", dfQtrans)
+        dc = Dict("Prad" => dfPrad ,"Pabs" => dfPabs , "Qtrans" => dfQtrans)
+        write_data(savefile[2], dc)
     end
-    return Pabs, Prad, qtrans
+    return wv, Pabs, Prad, qtrans
+end
+
+
+"""
+    simulation_data(filetype :: TotalFlux{:vsT},...)
+
+Return and save (optional) total heat_transfer as a function temparature of one of the bodies and transfer function.
+# Arguments
+- `Tmin` : minimum value of temperature of body 2
+- `Tmax` : maximum value of temperature of body 2
+- `T1 = 0.0` : temperature of body 1. Set by default at 0.0K
+"""
+function simulation_data(filetype :: TotalFlux{:vsT}, filename:: Union{String,Vector{String}}, columnname :: Array{Symbol,1},Tmin,Tmax, trans= "DEFAULT";T1=0.0, savefile = (false," "))
+    dfPabs,dfPrad  = import_data(filename ; transf = transf)
+    wv, Pabs, Prad = convert_to_array(dfPabs,dfPrad,columnname)
+
+    Tempv = collect(linspace(Tmin,Tmax,100))
+    q_tot = zeros(Float64,100)
+    tt(T) = total_transfer(T1,T,wv,Prad)
+    q_tot = tt.(Tempv)
+    Qtrans = [Tempv q_tot]
+
+    if savefile[1] == true
+        dfQtrans = DataFrame(Qtrans)
+        dc = Dict("Prad" => dfPrad ,"Pabs" => dfPabs , "Qtrans_vs_T_" => dfQtrans)
+        write_data(savefile[2], dc)
+    end
+    return wv, Pabs, Prad, Tempv, Qtrans
+end
+
+
+"""
+    simulation_data(filetype :: TotalFlux{:vsd},...)
+
+Return and save (optional) total heat_transfer as a function separation distance and transfer function for each separation distance.
+# Arguments
+- `T1` : Temperature of body 1
+- `T2` : Temperature of body 2
+- `trans` : vector containing the transformation label
+"""
+function simulation_data(filetype :: TotalFlux{:vsd}, filename :: Union{String,Vector{String}}, columnname :: Array{Symbol,1} ,T1,T2, trans; savefile = (false," "))
+    q_tot   = zeros(Float64,length(collect(trans)))
+
+    for i in trans
+        dfPabs,dfPrad  = import_data(filename ; transf = Float64(trans[i]))
+        wv, Pabs, Prad = convert_to_array(dfPabs,dfPrad,columnname)
+        q_tot[i]       = total_transfer(T1,T2,wv,Prad)
+        if savefile[1] == true
+            dc = Dict("Prad_trans="*"_$i" => dfPrad ,"Pabs_trans="*"_$i" => dfPabs)
+            write_data(savefile[2], dc)
+        end
+    end
+
+    Qtrans = [trans q_tot]
+
+    if savefile[1] == true
+        dfQtrans = DataFrame(Qtrans)
+        dc = Dict("Qtrans_vs_dist_" => dfQtrans)
+        write_data(savefile[2], dc)
+    end
+    return trans, q_trans
 end

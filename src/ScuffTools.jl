6b9082da99533a31bf1a1b1b4cbb77541dbab631
Olivier Merchiers <omerchiers@gmail.com>
2018-05-29 14:16:51 +0200

    added exported function

--- src/ScuffTools.jl
+++ src/ScuffTools.jl
@@ -8,6 +8,12 @@ using MultiLayerNFRHT
 # constants
 const w0 = 3.0e14
 
+# export of functions
+export import_data,simulation_data,
+       plot_scuff,benchmark_freq,
+       scuff_job,scuff_parallel,
+       transfer_w,total_transfer,frequency_file
+
 
 # Files
 include("plot/plotting.jl")
